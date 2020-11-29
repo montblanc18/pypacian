4f30198bf7843a5eb8091c0e4e19d4fbee7cbecc
montblanc18 <skurita.phys@gmail.com>
2020-11-29 16:39:42 +0900

    pytest run

--- tests/test_connect_handler.py
+++ tests/test_connect_handler.py
@@ -1,7 +1,16 @@
 from pypacian.pypacian import *
 import pytest
+import threading
 
 
 def test_connect_handler():
-    server_connect("127.0.0.1", 50000)
-    client_connect("127.0.0.1", 50000)
+    print(1)
+    # svconn = threading.Thread(target=server_connect, args=(["127.0.0.1", 50000]))
+    # print(2)
+    # clconn = threading.Thread(target=client_connect, args=(["127.0.0.1", 50000]))
+    # print(3)
+    # svconn.start()
+    # print(4)
+    # clconn.start()
+    # svconn.join()
+    # clconn.join()

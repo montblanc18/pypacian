a2206d60e7efb2ce60b61cb1ff92604bbf1119c9
montblanc18 <skurita.phys@gmail.com>
2020-11-29 16:25:32 +0900

    rewrite setup.py for executing pytest with Errors.

--- tests/test_connect_handler.py
+++ tests/test_connect_handler.py
@@ -1,6 +1,7 @@
-import pypacian.pypacian
+from pypacian.pypacian import *
+import pytest
 
 
 def test_connect_handler():
-    pypacian.server_connect("127.0.0.1", 50000)
-    pypacian.client_connect("127.0.0.1", 50000)
+    server_connect("127.0.0.1", 50000)
+    client_connect("127.0.0.1", 50000)
